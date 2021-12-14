c1ebee4773d93f9ca1008569310e140d243f678e
Ballantyne, Bob <ballantyne.robert@gmail.com>
2021-12-13 23:06:32 -0500

    add resolve

---
+++ vars/resolve.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+import hudson.model.Computer.ListPossibleNames
+
+def call(String prefix, String nodeName) {
+    def node = Jenkins.instance.getNode(nodeName)
+    def c = node.computer.getChannel()
+    if (c) {
+        c.call(new ListPossibleNames()).each { it ->
+            if (it.substring(0,5) == '10.3.') {
+               return prefix + it
+            }
+        }
+    }
+    return 'echo '
+}
