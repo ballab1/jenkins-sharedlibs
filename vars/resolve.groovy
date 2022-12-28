0b78d810ca5ef9839c67ad49549b78d24f9ef21c
Ballantyne, Bob <ballantyne.robert@gmail.com>
2022-12-27 21:02:38 -0500

    improve resolve.groovy

--- vars/resolve.groovy
+++ vars/resolve.groovy
@@ -12,5 +12,17 @@ def call(String prefix, String nodeName) {
             }
         }
     }
-    return prefix + nodeName
+
+    Map hosts = [ "raspberry": "pi.ubuntu.home",
+                  "ubuntu-s1": "s1.ubuntu.home",
+                  "ubuntu-s2": "s2.ubuntu.home",
+                  "ubuntu-s3": "s3.ubuntu.home",
+                  "ubuntu-s4": "s4.ubuntu.home",
+                  "ubuntu-s5": "s5.ubuntu.home",
+                  "ubuntu-s6": "s6.ubuntu.home",
+                  "ubuntu-s7": "s7.ubuntu.home",
+                  "ubuntu-s8": "s8.ubuntu.home"
+                ]
+
+    return prefix + hosts[nodeName]
 }
