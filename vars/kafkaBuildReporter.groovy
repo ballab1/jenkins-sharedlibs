f552d0de280a2af0a375b471244b02db76fdbc56
Ballantyne, Bob <ballantyne.robert@gmail.com>
2024-02-29 09:33:13 -0500

    update kafka modules

--- vars/kafkaBuildReporter.groovy
+++ vars/kafkaBuildReporter.groovy
@@ -1,20 +1,22 @@
 #!/usr/bin/env groovy
 
-@Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.2.11')
-@Grab(group = "org.apache.kafka", module = "kafka-clients", version = "3.1.0")
-import java.time.Instant
-import java.util.Date
+@Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.5.1')
+@Grab(group = "org.apache.kafka", module = "kafka-clients", version = "3.7.0")
+
 import org.apache.kafka.clients.producer.Callback
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.RecordMetadata
+
+import java.time.Instant
+import java.util.Date
 
 
 def call()
 {
     echo 'Publish to Kafka'
 
-    def props = [ 'bootstrap.servers': System.getenv('KAFKA_BOOTSTRAP_SERVERS') ?: 's3.ubuntu.home:9092,s4.ubuntu.home:9092,s8.ubuntu.home:9092',
+    def props = [ 'bootstrap.servers': System.getenv('KAFKA_BOOTSTRAP_SERVERS') ?: 's3.ubuntu.home:9092,s7.ubuntu.home:9092,s8.ubuntu.home:9092',
                   'key.serializer': 'org.apache.kafka.common.serialization.StringSerializer',
                   'value.serializer': 'org.apache.kafka.common.serialization.StringSerializer' ]
 
--- vars/kafkaNodes.groovy
+++ vars/kafkaNodes.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-    def env = System.getenv('KAFKA_BOOTSTRAP_SERVERS') ?: 's3.ubuntu.home:9092,s4.ubuntu.home:9092,s8.ubuntu.home:9092',
+    def env = System.getenv('KAFKA_BOOTSTRAP_SERVERS') ?: 's3.ubuntu.home:9092,s7.ubuntu.home:9092,s8.ubuntu.home:9092',
     def nodes = []
     env.split(',').each{ it ->
         def parts = it.split(':')
