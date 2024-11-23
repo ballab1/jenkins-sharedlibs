66d222fdf57a7026f90ee8c89461a62ea8a31e5d
Ballantyne, Bob <ballantyne.robert@gmail.com>
2024-11-23 11:10:22 -0500

    update groovy external libs

--- vars/kafkaBuildReporter.groovy
+++ vars/kafkaBuildReporter.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
-@Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.5.9')
-@Grab(group = "org.apache.kafka", module = "kafka-clients", version = "3.8.0")
+@Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.5.12')
+@Grab(group = "org.apache.kafka", module = "kafka-clients", version = "3.9.0")
 
 import org.apache.kafka.clients.producer.Callback
 import org.apache.kafka.clients.producer.KafkaProducer
