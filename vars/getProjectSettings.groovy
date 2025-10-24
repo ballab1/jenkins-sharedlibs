99e6b88494af08955d7554bde42e7504d036aab7
Ballantyne, Bob <ballantyne.robert@gmail.com>
2025-10-24 16:50:43 -0400

    update kaniko cmdline code

--- vars/getProjectSettings.groovy
+++ vars/getProjectSettings.groovy
@@ -1,28 +1,38 @@
 @Grab('org.yaml:snakeyaml:2.2')
 import org.yaml.snakeyaml.Yaml
+import java.io.File
+import hudson.FilePath
 
-// The getProjectSettings.groovy file defines a function to load and parse a YAML file, returning the settings as a map object
+//--------------------------------------------------------------------------------
+// The getProjectSettings.groovy file defines a function to load and parse a YAML file,
+// returning the settings as a map object
+//--------------------------------------------------------------------------------
 
-def call(String settingsFile = 'ci/project.settings') {
+def call(String settings = 'project.settings') {
 
+    def settingsFile = settings
     // Load YAML file
     try {
-        def yamlFile = new File(settingsFile)
-        if (!yamlFile.exists()) {
-            throw new FileNotFoundException("YAML file '${settingsFile}' not found.")
+        if (! fileExists(settingsFile) && fileExists('ci/' + settingsFile)) {
+            settingsFile = 'ci/' + settingsFile
         }
-        if (yamlFile.isdir()) {
-            throw new Exception("Invalid YAML file '${settingsFile}'. Directory provided.")
-        }
-
+        def file = new File(settingsFile)
+//        def yamlFile = new FilePath(file)
+//        if (! yamlFile.exists()) {
+//            throw new FileNotFoundException("YAML file '${settings}' not found.")
+//        }
+//        if (yamlFile.isDirectory()) {
+//            throw new Exception("Invalid YAML file '${settings}'. Directory provided.")
+//        }
         // Parse YAML
         def yaml = new Yaml()
-        return yaml.load(yamlFile.text)
+//        return yaml.load(yamlFile.readToString())
+        return yaml.load(file.text)
     }
     catch(e) {
-        println "unable to open project settings file: '${settingsFile}'\nreason: ${e.getMessage()}"
-        return null
+        println "getProjectSettings: Possible issue parsing project settings file: '${settingsFile}'\n    reason: ${e.getMessage()}"
     }
-    println "Possible issue parsing project settings file: '${settingsFile}'"
+    manager.addErrorBadge('Unable to local project settings')
+    manager.buildFailure()
     return null
 }
