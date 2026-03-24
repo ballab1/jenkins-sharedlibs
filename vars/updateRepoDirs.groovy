f1359840a8c4cf03edc67a7cbcf9af2370cd53f4
Ballantyne, Bob <ballantyne.robert@gmail.com>
2026-03-24 16:24:37 -0400

    updates to updateRepoDirs

--- vars/updateRepoDirs.groovy
+++ vars/updateRepoDirs.groovy
@@ -1,27 +1,35 @@
-#!/usr/bin/env groovy
+@Grab('org.yaml:snakeyaml:2.2')
+import org.yaml.snakeyaml.Yaml
 
 import groovy.json.*
 
 def call(String dirFileSpec) {
 
-    def jsonFile = new File(dirFileSpec)
-    if (! jsonFile.exists()) {
-        println 'unable to find '+jsonFile.absolutePath+'...'
+    if (! fileExists(dirFileSpec)) {
+        println 'updateRepoDirs: unable to find ' + dirFileSpec.absolutePath + '...'
         return
     }
 
-    println 'found '+jsonFile.absolutePath
+    try {
+        def file_text = readFile(dirFileSpec)
 
-    def slurper = new JsonSlurper()
-    def json = slurper.parseText(jsonFile.text)
+        println 'found ' + dirFileSpec.absolutePath
 
-    json.each { nodeName, dirs ->
-        def out = new File("${WORKSPACE}/${nodeName}.dirs")
-        if ( out.exists() ) {
-            out.delete()
+        // Parse YAML
+        def yaml = new Yaml()
+        def json = yaml.load(file_text)
+
+        json.each { nodeName, dirs ->
+            def out = new File("${WORKSPACE}/${nodeName}.dirs")
+            if ( out.exists() ) {
+               out.delete()
+            }
+            dirs.each { dir ->
+               out << dir + '\n'
+            }
         }
-        dirs.each { dir ->
-            out << dir + '\n'
-        }
+    }
+    catch(e) {
+        println 'updateRepoDirs: unable to parse ' + dirFileSpec.absolutePath + '...\n' + "    reason: ${e.getMessage()}"
     }
 }
